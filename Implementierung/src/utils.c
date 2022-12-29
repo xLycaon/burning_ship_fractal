c3982fac3d214b350407320792d020823ef212c6
Giancarlo Calvache Valladares <cag@in.tum.de>
2022-12-29 16:17:10 +0100

    reverse order of writing bmp file lines

--- Implementierung/src/utils.c
+++ Implementierung/src/utils.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #ifdef __APPLE__
 #include <libkern/OSByteOrder.h>
@@ -14,12 +13,11 @@
 
 #include "utils.h"
 
-//TODO inline? then without malloc?
 static void*
 gscale_img24bmp(unsigned char* img, BMP_H bmph) {
-	unsigned char* n_img; //TODO malloc clean?
-	if ( (n_img = malloc(bmph.img_width*BYTESPP * bmph.img_height*BYTESPP)) == NULL ) //TODO failure handling here or in main
-		exit(EXIT_FAILURE);
+	unsigned char* n_img;
+	if ( (n_img = malloc(bmph.img_width*BYTESPP * bmph.img_height*BYTESPP)) == NULL )
+		return NULL;
 
 	for (size_t y = 0; y < bmph.img_height; y++) {
 		for (size_t x = 0; x < bmph.img_width; x++) {
@@ -33,18 +31,16 @@ gscale_img24bmp(unsigned char* img, BMP_H bmph) {
 	return n_img;
 }
 
-//TODO scaling of BMP must also be handled in main
-void
+ssize_t
 writef_bmp(unsigned char* img, const char* path, BMP_H bmph) {
 	FILE* file;
-	if ( (file = fopen(path, "w")) == NULL ) //TODO failure handling here or in main
-		exit(EXIT_FAILURE);
+	if ( (file = fopen(path, "w")) == NULL ) 
+		return -1;
+	ssize_t written = 0;
 
 	// Write BMP HEADER into file
-	fwrite((char *) &bmph, 1, sizeof (BMP_H), file);
+	written += fwrite((char *) &bmph, 1, sizeof (BMP_H), file);
 
-	// Write IMAGE DATA into file
-	
 	// Set padding to MAX 3 (PAD_ALIGN - 1)
 	unsigned char npad = (PAD_ALIGN - (bmph.img_width*BYTESPP % PAD_ALIGN));
 	npad = npad % PAD_ALIGN ? npad : 0;
@@ -52,22 +48,25 @@ writef_bmp(unsigned char* img, const char* path, BMP_H bmph) {
 	memset(zeros, 0, npad);
 
 	// Scale w x h img to 24bbp bmp (w*3) x (h*3) greyscale
-	unsigned char* img_scld = gscale_img24bmp(img, bmph);
+	unsigned char* img_scld;
+	if ( (img_scld = gscale_img24bmp(img, bmph)) == NULL )
+		return -1;
 
-	for (size_t y = 0; y < bmph.img_height; y++) {
-		fwrite(&img_scld[y * bmph.img_width*BYTESPP], 1, bmph.img_width*BYTESPP, file);
-		// ROW PADDING
-		fwrite(zeros, 1, npad, file);
+	// Write IMAGE DATA into file
+	for (size_t y = bmph.img_height-1; (y+1) > 0; y--) {
+		written += fwrite(&(img_scld[y * bmph.img_width*BYTESPP]), 1, bmph.img_width*BYTESPP, file);
+		written += fwrite(zeros, 1, npad, file); // ROW PADDING
 	}
 	free(img_scld);
+
+	return written;
 }
 
-//TODO big endian to little endian if parameters are larger than max number range for 1 byte
-//TODO for example in fsize
+//TODO endianess
 BMP_H
-creat_bmph(size_t img_w, size_t img_h) { // bswap for big endian -> little endian
+creat_bmph(size_t img_w, size_t img_h) { 
 	BMP_H bmp;
-	bmp.magic = bswap_16(MAGIC);
+	bmp.magic = bswap_16(MAGIC); // bswap for big endian -> little endian
 	bmp.fsize = img_w*BYTESPP * img_h*BYTESPP + sizeof (BMP_H);
 	bmp.reserved = 0;
 	bmp.dib_offset = sizeof (BMP_H);
