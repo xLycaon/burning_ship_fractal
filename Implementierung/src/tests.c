003fefe9ac981a22776758f3d52e5571ad399257
Emre Oeztas <oeztase@in.tum.de>
2023-01-28 19:33:46 +0100

    test percentage

--- Implementierung/src/tests.c
+++ Implementierung/src/tests.c
@@ -10,7 +10,8 @@ void test_image_sanity(burning_ship_t bs1, burning_ship_t bs2, struct BS_Params 
 //TODO introduce epsilon for inaccuracy of pixel values
 void test_image_sanity(burning_ship_t bs1, burning_ship_t bs2, struct BS_Params params) { //TODO reset cache?
     unsigned char *img1, *img2;
-
+    size_t different_pixels;
+    double difference_percentage;
     size_t i_size = params.width * params.height;
 
     if ( (img1 = malloc(i_size)) == NULL ) {
@@ -24,11 +25,12 @@ void test_image_sanity(burning_ship_t bs1, burning_ship_t bs2, struct BS_Params 
     bs1(params.start, params.width, params.height, params.res, params.n, img1);
     bs2(params.start, params.width, params.height, params.res, params.n, img2);
 
-    if (memcmp(img1, img2, i_size) != 0) {
-        fprintf(stderr, "TEST: Image sanity DID NOT pass!\n");
-    } else {
-        printf("TEST: Image sanity passed!\n");
-    }
+    different_pixels = memcmp(img1, img2, i_size);
+    difference_percentage = (double) different_pixels / i_size * 100;
+    if(difference_percentage > 0.0)
+        printf("TEST: Image difference is %lf%%\n", difference_percentage);
+    else
+        printf("TEST: Images are equal!\n");
 
     free(img1);
     free(img2);
